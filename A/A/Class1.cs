92a01bed12d54b953f518b34979763fa7b14cb01
francois.genois <francois.genois@wolterskluwer.com>
2016-01-15 13:20:27 -0500

    Wildcarded source files

--- A/A/Class1.cs
+++ A/A/Class1.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using E;
+using F;
 
 namespace A
 {
     public class Class1
     {
+        private FClass e = new FClass();
+        private EClass f = new EClass();
     }
 }
