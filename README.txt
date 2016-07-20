b363bf2d50031e71ff9bb3a39ca471b44b64f8c7
francois.genois <francois.genois@wolterskluwer.com>
2016-07-20 13:39:39 -0400

    paket update issue

--- README.txt
+++
@@ -1,7 +0,0 @@
-This is a repro sample for https://github.com/fsprojects/Paket/issues/1316
-
-Repro steps :
-1. Run setup.bat (that'll download the paket.exe)
-2. Run problematic_command.bat and look at output
-
-I left some .nupkg in addition to their project in the repository to serve as examples.
---
+++ Sandbox2.0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Collections.Generic;
+
+using System.Data.SqlTypes;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Sandbox2._0
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
